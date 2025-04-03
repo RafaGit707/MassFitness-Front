874de4afea605142e716ce97f9a26ce2336dba43
RafaGit707 <rgalcal303@g.educaand.es>
2025-04-03 15:19:12 +0200

    Implementando Rol, Eliminando Datos Personales, Implementacion y Correccion de Security

--- app/src/main/java/com/example/massfitness/AdminAddActivity.java
+++ app/src/main/java/com/example/massfitness/AdminAddActivity.java
@@ -2,12 +2,14 @@ package com.example.massfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class AdminAddActivity extends AppCompatActivity {
-    private ImageView ivBack;
+    private ImageView ivBack, addClase, addEntrenador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +17,26 @@ public class AdminAddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_add);
 
         ivBack = findViewById(R.id.ivBack);
+        addEntrenador = findViewById(R.id.addEntrenador);
+        addClase = findViewById(R.id.addClase);
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        addEntrenador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminAddActivity.this, AddEntrenadorActivity.class));
+            }
+        });
+
+        addClase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminAddActivity.this, AddClaseActivity.class));
             }
         });
     }
